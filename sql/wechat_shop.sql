c6aa6c6b42117e4005c9236ade601f6fdb28a769
servanter <fengshang@126.com>
2014-11-17 15:49:29 +0800

    Update the sql.

--- sql/wechat_shop.sql
+++ sql/wechat_shop.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50519
 File Encoding         : 65001
 
-Date: 2014-11-17 10:11:02
+Date: 2014-11-17 15:48:43
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -44,9 +44,47 @@ CREATE TABLE `shop` (
   `create_time` timestamp NOT NULL DEFAULT '1990-01-01 00:00:00',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of shop
 -- ----------------------------
 INSERT INTO `shop` VALUES ('1', '新品会', 'a阿斯顿大飒飒的按时按时', '2014-11-13 16:27:07', '2014-11-13 16:27:07');
+INSERT INTO `shop` VALUES ('2', '京东', '阿斯顿记录卡见识到了卡机顺路快递将阿里卡', '1990-01-01 00:00:00', '2014-11-17 11:42:39');
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(20) NOT NULL,
+  `user_pass` varchar(20) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT '1990-01-01 00:00:00',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_vertify` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'aaa', '123', '1990-01-01 00:00:00', '2014-11-17 10:59:10', '1');
+
+-- ----------------------------
+-- Table structure for `user_shop`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_shop`;
+CREATE TABLE `user_shop` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `shop_id` bigint(20) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_shop
+-- ----------------------------
+INSERT INTO `user_shop` VALUES ('1', '1', '1', '2014-11-17 11:29:31');
+INSERT INTO `user_shop` VALUES ('2', '1', '2', '2014-11-17 11:43:16');
