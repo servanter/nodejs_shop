d62a24959a763676f138fbac4f70eef5b3a8f5e5
servanter <fengshang@126.com>
2015-01-09 11:00:07 +0800

    Format the code.

--- service/itemservice.js
+++ service/itemservice.js
@@ -16,25 +16,40 @@ exports.findItemsIndexPositionsByShopId = function(shopId, paging, callback) {
             findPositions(shopId, 1, paging, function(data) {
                 cb(null, data);
             })
-        }, function(data, cb) {
-            Item.count({include:[{model:Position, required:true, as : 'position', where:{position:1, is_del:0}}], where:{shop_id:shopId}}).success(function(count) {
+        },
+        function(data, cb) {
+            Item.count({
+                include: [{
+                    model: Position,
+                    required: true,
+                    as: 'position',
+                    where: {
+                        position: 1,
+                        is_del: 0
+                    }
+                }],
+                where: {
+                    shop_id: shopId
+                }
+            }).success(function(count) {
                 var pag = new Paging(count, paging.page, paging.pageSize, data);
                 cb(null, pag);
             })
         }
-        ], function(err, results) {
-            if(err) {
-                throw err;
-            } else {
-                callback(results);
-            }
+    ], function(err, results) {
+        if (err) {
+            throw err;
+        } else {
+            callback(results);
         }
-    )
+    })
 }
 
 exports.search = function(shopId, params, paging, callback) {
-    var whereConditions = {shop_id:shopId};
-    if(params.a && params.a != '0') {
+    var whereConditions = {
+        shop_id: shopId
+    };
+    if (params.a && params.a != '0') {
         var category = parseInt(params.a);
         var subService = new itemSubFactory.getService(category);
         subService.findList(shopId, params, paging, function(result) {
@@ -48,28 +63,35 @@ exports.search = function(shopId, params, paging, callback) {
         // default
         async.waterfall([
             function(cb) {
-                Item.findAll({where:whereConditions, offset:paging.sinceCount, limit:paging.pageSize}).success(function(data){
+                Item.findAll({
+                    where: whereConditions,
+                    offset: paging.sinceCount,
+                    limit: paging.pageSize
+                }).success(function(data) {
                     var arr = Convert.values2Arr(data);
                     cb(null, arr);
                 });
-            }, function(data, cb) {
-                Item.count({where:whereConditions}).success(function(count) {
+            },
+            function(data, cb) {
+                Item.count({
+                    where: whereConditions
+                }).success(function(count) {
                     var pag = new Paging(count, paging.page, paging.pageSize, data);
                     cb(null, pag);
                 })
-            }], function(err, result) {
-                if(err) {
-                    throw err;
-                } else {
-                    result.result.forEach(function(item, index) {
-                        item.encrypt = Crypto.encryptAes(item.detail_id + Constants.cryptoSplit + item.class_id);
-                    })
-                    callback(result);
-                }
             }
-        )
+        ], function(err, result) {
+            if (err) {
+                throw err;
+            } else {
+                result.result.forEach(function(item, index) {
+                    item.encrypt = Crypto.encryptAes(item.detail_id + Constants.cryptoSplit + item.class_id);
+                })
+                callback(result);
+            }
+        })
     }
-    
+
 }
 
 exports.findClasses = function(callback) {
@@ -82,7 +104,7 @@ exports.findClasses = function(callback) {
 
 exports.save = function(item, callback) {
     Item.create(item).complete(function(err, data) {
-        if(err) {
+        if (err) {
             callback(false);
         } else {
             callback(true);
@@ -93,28 +115,64 @@ exports.save = function(item, callback) {
 exports.findById = function(id, callback) {
     async.waterfall([
         function(cb) {
-            Item.findOne({where:{id:id}}, {subQuery:false}).success(function(data) {
+            Item.findOne({
+                where: {
+                    id: id
+                }
+            }, {
+                subQuery: false
+            }).success(function(data) {
                 cb(null, data);
             })
-        }, function(data, cb) {
+        },
+        function(data, cb) {
             var subFactory = new itemSubFactory.getService(parseInt(data.class_id));
-            if(subFactory) {
+            if (subFactory) {
                 subFactory.findDetail(data.dataValues.id, function(result) {
-                    cb(null, {item:data.dataValues, detail:result});
+                    cb(null, {
+                        item: data.dataValues,
+                        detail: result
+                    });
                 });
 
             }
         }
-        ], function(err, results) {
-            callback(results);
-        })
+    ], function(err, results) {
+        callback(results);
+    })
 }
 
-exports.findItemClassesByShopId = function (shopId, callback) {
-    Item.findAll({attributes:[[Sequelize.fn('COUNT', 'class_id'), 'total']], include:[{model:ItemClass, required:true, as:'category', attributes:[['alias', 'alias'],['id', 'id'] ]}], where:{shop_id:shopId}, group:['class_id'], order:[[Sequelize.fn('COUNT', 'class_id'), 'DESC']]}, {subQuery:false}).success(function(result) {
+exports.findItemClassesByShopId = function(shopId, callback) {
+    Item.findAll({
+        attributes: [
+            [Sequelize.fn('COUNT', 'class_id'), 'total']
+        ],
+        include: [{
+            model: ItemClass,
+            required: true,
+            as: 'category',
+            attributes: [
+                ['alias', 'alias'],
+                ['id', 'id']
+            ]
+        }],
+        where: {
+            shop_id: shopId
+        },
+        group: ['class_id'],
+        order: [
+            [Sequelize.fn('COUNT', 'class_id'), 'DESC']
+        ]
+    }, {
+        subQuery: false
+    }).success(function(result) {
         var arr = [];
         for (var i = 0; i < result.length; i++) {
-            arr.push({name:result[i].category.alias, link:'a' + result[i].category.id + 'b0c0d0', total:result[i].dataValues.total});
+            arr.push({
+                name: result[i].category.alias,
+                link: 'a' + result[i].category.id + 'b0c0d0',
+                total: result[i].dataValues.total
+            });
         }
         callback(arr);
     })
@@ -123,13 +181,30 @@ exports.findItemClassesByShopId = function (shopId, callback) {
 exports.findAttributesByClassId = function(classId, callback) {
     var subFactory = new itemSubFactory.getService(parseInt(classId));
     subFactory.findFullConditions(function(result) {
-        callback(result);    
+        callback(result);
     })
 }
 
 
-function findPositions (shopId, p, paging, callback)  {
-    Item.findAll({include:[{model:Position, as : 'position', required:true, where:{position:p, is_del:0}}], where:{shop_id:shopId}, offset:paging.sinceCount, limit:paging.pageSize}, {subQuery:false}).success(function(data){
+function findPositions(shopId, p, paging, callback) {
+    Item.findAll({
+        include: [{
+            model: Position,
+            as: 'position',
+            required: true,
+            where: {
+                position: p,
+                is_del: 0
+            }
+        }],
+        where: {
+            shop_id: shopId
+        },
+        offset: paging.sinceCount,
+        limit: paging.pageSize
+    }, {
+        subQuery: false
+    }).success(function(data) {
         data.forEach(function(item, index) {
             item.encrypt = Crypto.encryptAes(item.detail_id + Constants.cryptoSplit + item.class_id + Constants.cryptoSplit + item.position.item_id);
         })
@@ -138,23 +213,39 @@ function findPositions (shopId, p, paging, callback)  {
 }
 exports.findPositions = findPositions;
 
-exports.findCurrentPositionsAndAvaliableItemNames = function (shopId, position, paging, callback) {
+exports.findCurrentPositionsAndAvaliableItemNames = function(shopId, position, paging, callback) {
     async.waterfall([
         function(cb) {
             findPositions(shopId, position, paging, function(data) {
                 cb(null, data);
             })
-        }, function (data, cb) {
-            Item.findAll({attributes:[['id', 'id'],['short_name', 'short_name'], ['pic_url', 'pic_url'], ['detail_id', 'detail_id'], ['class_id', 'class_id']], where:{shop_id:shopId}}).success(function(result){
+        },
+        function(data, cb) {
+            Item.findAll({
+                attributes: [
+                    ['id', 'id'],
+                    ['short_name', 'short_name'],
+                    ['pic_url', 'pic_url'],
+                    ['detail_id', 'detail_id'],
+                    ['class_id', 'class_id']
+                ],
+                where: {
+                    shop_id: shopId
+                }
+            }).success(function(result) {
                 result.forEach(function(item, index) {
                     item.encrypt = Crypto.encryptAes(item.id + '');
-                    
+
                 })
-                cb(null, {current:data, items:result});
+                cb(null, {
+                    current: data,
+                    items: result
+                });
             });
-        }], function(err, result) {
-            callback(result);
-        })
+        }
+    ], function(err, result) {
+        callback(result);
+    })
 }
 
 exports.removePositions = function(shopId, position, ids, callback) {
@@ -164,8 +255,17 @@ exports.removePositions = function(shopId, position, ids, callback) {
         var message = Crypto.decryptAes(item + '')
         idArr.push(message.split(Constants.cryptoSplit)[2]);
     })
-    Position.update({is_del : 1}, {where:{shop_id : shopId, position : position, item_id : { in :idArr}}}).complete(function(err, result) {
-        if(result > 0) {
+    Position.update({
+        is_del: 1
+    }, {
+        where: {
+            shop_id: shopId,
+            position: position,
+            item_id: { in : idArr
+            }
+        }
+    }).complete(function(err, result) {
+        if (result > 0) {
             callback(result);
         } else {
             callback(result);
@@ -176,26 +276,30 @@ exports.removePositions = function(shopId, position, ids, callback) {
 
 exports.findBaseInfoById = function(encrypt, callback) {
     var id = Crypto.decryptAes(encrypt + '');
-    Item.findOne({where:{id:id}}).success(function(result) {
-        result.setDataValue('encrypt',Crypto.encryptAes(result.detail_id + Constants.cryptoSplit + result.class_id + Constants.cryptoSplit + result.id));
+    Item.findOne({
+        where: {
+            id: id
+        }
+    }).success(function(result) {
+        result.setDataValue('encrypt', Crypto.encryptAes(result.detail_id + Constants.cryptoSplit + result.class_id + Constants.cryptoSplit + result.id));
         callback(result);
     })
 }
 
 exports.addIndexPosition = function(shopId, encrypt, userId, index, callback) {
     var id = Crypto.decryptAes(encrypt + '');
-    if(id) {
+    if (id) {
         model.Position.create({
-            shop_id : shopId,
-            item_id : id,
-            user_id : userId,
+            shop_id: shopId,
+            item_id: id,
+            user_id: userId,
             position: index
 
         }).complete(function(err, result) {
-            if(err) {
+            if (err) {
                 callback(false);
             } else {
-                callback(result.id);    
+                callback(result.id);
             }
         })
     }
